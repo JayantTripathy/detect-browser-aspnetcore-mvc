7c420c059c0f06e660fa4fabbbd13e4f7609197f
jayant <jayanttripathy1985@gmail.com>
2022-12-08 10:36:39 +0530

    Add project files.

---
+++ detect-browser-aspnetcore-mvc/Models/Browser.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+namespace detect_browser_aspnetcore_mvc.Models
+{
+    public class Browser
+    {
+        public string userAgent { get; set; }
+        public Regex OS { get; set; }
+        public Regex device { get; set; }
+    }
+}
